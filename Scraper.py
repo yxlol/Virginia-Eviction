878093f4842c795a4abc83f311d10c2d929b4dff
AntiNita <yxiao03@wm.edu>
2022-05-08 09:29:57 -0400

    Add File

--- Scraper.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.firefox.service import Service
-from webdriver_manager.firefox import GeckoDriverManager
-
-driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
-
-driver.get("http://ewsocis1.courts.state.va.us/CJISWeb/circuit.jsp")
